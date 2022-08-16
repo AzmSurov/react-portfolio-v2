9119f4ed6667a9a0e0d39846487fe8900e6014fc
AZM Mohibul Surov <azmsm1@AZMs-MacBook-Pro.local>
2022-08-16 00:02:57 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='dark:bg-black'>
     <App />
-    </div>
   </React.StrictMode>
 );
 

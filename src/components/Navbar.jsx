defc84978e9e493059dcbc979749e35ce33d14c4
AZM Mohibul Surov <azmsm1@AZMs-MacBook-Pro.local>
2022-08-25 01:52:03 -0500

    k

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = ({ setSendData }) => {
     <div>
       <header className="mb-4">
         <div className="flex justify-between items-center px-4 py-6">
-          <div className="w-10 ">
+          <div className="">
           <div class="relative inline-flex group ">
                                   <div class="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#19A186] to-[#F2CF43]  blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
 
